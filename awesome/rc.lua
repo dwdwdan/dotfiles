32f86426617de12604c81c9337b922e83a660f17
dwdwdan <dan.walters5@outlook.com>
2021-05-03 18:18:01 +0100

    Awesome - Removed menu button on right and layout selector on left

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -232,7 +232,6 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
             s.mytaglist,
             s.mypromptbox,
         },
@@ -241,7 +240,6 @@ awful.screen.connect_for_each_screen(function(s)
             layout = wibox.layout.fixed.horizontal,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
