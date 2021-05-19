60cdef5c0c107ba64da1ec067103102a09b3f79d
dwdwdan <dan.walters5@outlook.com>
2021-05-19 19:31:55 +0100

    Awesome - battery widget now displays charge status
    An up arrow is used for charging, a down arrow for discharging and no
    symbol is displayed when full

--- awesome/my-widgets/battery.lua
+++ awesome/my-widgets/battery.lua
@@ -13,7 +13,15 @@ bat_widget:set_widget(bat_text)
 bat_widget:set_bg(theme.bg_widget)
 bat_widget:set_fg(theme.fg_widget)
 
-watch("acpi",60,function(widget, stdout, stderr, exitreason, exitcode)
+watch("acpi",10,function(widget, stdout, stderr, exitreason, exitcode)
+	local bat_status = ""
+	if(string.find(stdout,"Dis")) then
+		bat_status = "⬇"
+	elseif(string.find(stdout,"Full")) then
+		bat_status = " "
+	else
+		bat_status = "⬆"
+	end
 	local bat_percent = string.match(stdout,"%d?%d?%d%%")
 	bat_percent = bat_percent:sub(1,-2)
 	if(tonumber(bat_percent) < 100) then
@@ -22,9 +30,8 @@ watch("acpi",60,function(widget, stdout, stderr, exitreason, exitcode)
 	if(tonumber(bat_percent) < 10) then
 		bat_percent = " "..bat_percent
 	end
-	bat_percent = "🔋"..bat_percent
 	bat_percent = bat_percent.."%"
-	bat_text:set_text(bat_percent)
+	bat_text:set_text("🔋"..bat_status..bat_percent)
 end, bat_widget)
 
 return bat_widget
