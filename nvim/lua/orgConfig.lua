2644c4a0fa1ed98c754672055504c90a65fc7e16
dwdwdan <dan.walters5@outlook.com>
2021-07-01 12:55:26 +0100

    Added capture templates

--- nvim/lua/orgConfig.lua
+++ nvim/lua/orgConfig.lua
@@ -1,5 +1,17 @@
 require('orgmode').setup{
 	org_agenda_files = '~/org/*',
 	org_default_notes_file = '~/org/refile.org',
-	org_todo_keywords = {'TODO', '|', 'DONE', 'CANCELLED', 'CHECKED'}
+	org_todo_keywords = {'TODO', '|', 'DONE', 'CANCELLED', 'CHECKED'},
+	org_agenda_templates = {
+		t = {
+			description = 'Task',
+			template = '* TODO %?\n  %U',
+			target = '~/org/todo.org'
+		},
+		r = {
+			description = 'Reference',
+			template = '* %?\n',
+			target = '~/org/reference.org'
+		}
+	}
 }
