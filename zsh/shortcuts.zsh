118564044a8538540ad968d7716660a4484ccded
dwdwdan <dan.walters5@outlook.com>
2021-07-07 14:30:03 +0100

    Adjusted aliases to autorun certain things and create tmux sessions
    dotfiles and web aliases are now functions which will open a tmux
    session. web will also run the website locally

---
+++ zsh/shortcuts.zsh
@@ -0,0 +1,45 @@
+dotfiles(){
+	# Set Session Name
+	SESSION="dotfiles"
+	SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+	# Only create tmux session if it doesn't already exist
+	if [ "$SESSIONEXISTS" = "" ]
+	then
+		# Start New Session with our name
+		tmux new-session -d -s $SESSION
+
+		 # Name first Pane and start zsh
+		 tmux rename-window -t 0 'Main'
+		 tmux send-keys -t 'Main' 'cd ~/repos/dotfiles' C-m 'neofetch' C-m
+
+	fi
+
+	# Attach Session, on the Main window
+	tmux attach-session -t $SESSION:0
+# Adapted from http://ryan.himmelwright.net/post/scripting-tmux-workspaces/
+}
+
+web(){
+	# Set Session Name
+	SESSION="website"
+	SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+	# Only create tmux session if it doesn't already exist
+	if [ "$SESSIONEXISTS" = "" ]
+	then
+		# Start New Session with our name
+		tmux new-session -d -s $SESSION
+
+		 # Name first Pane and start zsh
+		 tmux rename-window -t 0 'Main'
+		 tmux send-keys -t 'Main' 'cd ~/repos/website' C-m 'clear' C-m
+
+		 tmux new-window -t $SESSION:1 -n 'Jekyll Server'
+		 tmux send-keys -t 'Jekyll Server' 'cd ~/repos/website; bundle exec jekyll serve --livereload' C-m
+	fi
+
+	# Attach Session, on the Main window
+	tmux attach-session -t $SESSION:0
+# Adapted from http://ryan.himmelwright.net/post/scripting-tmux-workspaces/
+}
