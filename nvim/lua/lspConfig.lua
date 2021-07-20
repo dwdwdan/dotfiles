676880d8f08c607eddd399380f8829783004a634
dwdwdan <dan.walters5@outlook.com>
2021-07-20 11:40:15 +0100

    Further lsp setup

--- nvim/lua/lspConfig.lua
+++ nvim/lua/lspConfig.lua
@@ -86,7 +86,9 @@ nvim_lsp.html.setup{
 	on_attach = on_attach,
 	capabilities = capabilities,
 }
-nvim_lsp.cssls.setup {
+nvim_lsp.cssls.setup{
 	on_attach = on_attach,
   capabilities = capabilities,
 }
+
+nvim_lsp.pyright.setup{on_attach = on_attach}
