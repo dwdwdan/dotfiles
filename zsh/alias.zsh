2f93466eb6626a3818ceab4c98f8635841b6e4ee
dwdwdan <dan.walters5@outlook.com>
2021-08-08 13:04:33 +0100

    Tidied up alias file

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,20 +1,25 @@
-alias rm="mv -t ~/.trash/"
+# Vim aliases
 alias v=nvim
 alias vim=nvim
 alias vf="v \$(fzf-tmux)"
-alias q="exit"
+
+# Git aliases
 alias g="git"
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
-alias bashrc="source ~/.bashrc"
 alias zshrc="source ~/.zshrc"
+
+# Ls aliases
 alias ls="exa --icons --all"
 alias ll="exa --icons --long --header --git --across --all --no-user"
 alias l="exa --icons --long --header --git --across --all --no-user"
 alias lg="ll | grep "
+
+# Other aliases
 alias run="screen -d -m"
 alias p="sudo pacman"
 alias neofetch="clear;neofetch;truecolor-test"
 alias startmpd="mpd; run mpDris2; ncmpcpp"
-alias tas="tmux_cond_attach"
+alias q="exit"
+alias rm="mv -t ~/.trash/"
